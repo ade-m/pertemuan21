d53ed49462983e29d3db3fccda7332af2132ffec
ade-m <66313509+ade-m@users.noreply.github.com>
2022-05-31 11:12:37 +0700

    Inputan keyboard

--- src/App.java
+++ src/App.java
@@ -1,17 +1,22 @@
 import model.lingkaran;
-
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         String pesan = "Selamat Datang di Kuliah PBO ";
         int tahun = 2022;
         System.out.println(pesan + tahun);
         System.out.print("============================\n");
-       
+        //definisi variabel untuk menerima masukan keyboard
+        Scanner keyboard = new Scanner(System.in);
+
         lingkaran lingkaran1 = new lingkaran();
-        lingkaran1.r=12;
+
+        System.out.print("Masukkan Panjang jari-jari (r) : ");
+        //terima masukan keyboard dari scanner ke r
+        lingkaran1.r= keyboard.nextInt();
+        //panggil luas lingkaran
         lingkaran1.luasLingkaran();
         
-
     }
 
 }
